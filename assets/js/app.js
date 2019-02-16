6f937e3565d4a8abf3218d7aad7356371d0867bb
Rafael Ferreira <rsferreira11@gmail.com>
2019-02-16 22:19:36 +0200

    Make Index Page | Make Chapter 3 to work on Phoenix 1.4

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,12 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import '../vendor/css/font-awesome.min.css'
+import '../vendor/css/phoenix.css'
+import "../css/app.css"
+
+// import '../vendor/js/jquery-3.2.1.min.js'
+// import '../vendor/js/bootstrap.min.js'
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -4,6 +4,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+const jqueryPath = path.resolve(__dirname, './vendor/js/jquery-3.2.1.min.js');
 
 module.exports = (env, options) => ({
   optimization: {
@@ -31,11 +34,25 @@ module.exports = (env, options) => ({
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: '../fonts'
+            }
+        }]
       }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
+    new CopyWebpackPlugin([{ from: 'static/', to: '../' }]),
+    new webpack.ProvidePlugin({
+      $: jqueryPath,
+      jQuery: jqueryPath
+    })
   ]
 });
